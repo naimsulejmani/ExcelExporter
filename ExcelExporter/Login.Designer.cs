004592f7e6976033fac011a013457fc05d9607b7
keds13136 <keds13136@KEDS-LPT13136>
2019-03-25 11:50:45 +0100

    login load

--- ExcelExporter/Login.Designer.cs
+++ ExcelExporter/Login.Designer.cs
@@ -200,6 +200,7 @@
             this.MinimizeBox = false;
             this.Name = "Login";
             this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
